2af90ba9ab98b25c4d8962879992fbb7c2bfdfc3
Palash A <pa334@cornell.edu>
2020-03-20 04:46:59 -0400

    only jq left

--- draft.sh
+++ draft.sh
@@ -1,47 +1,35 @@
 #!/usr/bin/env bash
 
+
 # start #
 [ $# -eq "1" ] && dir=$1 || echo "please supply parent directory"
 allsoft="$dir/modulefiles/mugqic"
 
-# temp files and dirs #
-[ -d "json-temp" ] || mkdir json-temp
-[ -f "current.json" ] || touch current.json
 
 # gather info #
 declare -a all
-declare -a ver
-
 declare -A obj
 
-all=( $(ls $allsoft) )
-for soft in ${all[@]}; do
-  ver=( $(ls -F "$allsoft/$soft") )
-  curr=$(cat "$allsoft/$soft/.version" | grep \" | awk '{print $NF}')
-  # .version format :: set ModulesVersion "<version-num>"
-  declare -A temp
-  temp[name]=$soft
-  temp[versions]=$ver
-  temp[current]=$curr
-  temp[notes]=""
-  obj+=([$soft]=$temp)
-  for i in ${temp[@]}; do
-    [ `declare -p ${1} 2> /dev/null | grep 'declare \-a'` ] \
-      && echo ${!i[@]} || echo $i
-  done
-  echo ""
+all=( $(ls $allsoft) ) && len=${#all[@]}; i=0
+for soft in ${all[@]}; do ((i++))
+  vers=`ls -F "$allsoft/$soft" | tr '\n' ',' | sed -e 's/,$//' -e 's/,/ | /g'`
+  curr=`cat "$allsoft/$soft/.version" | grep \" | awk '{print $NF}' | \
+    sed -e 's/^"//' -e 's/"$//'`
+  obj["$i,name"]=$soft; obj["$i,versions"]=$vers; obj["$i,current"]=$curr
+  obj["$i,notes"]="based on information given. needs human intervention."
 done
 
 
 # write to json #
-
+[ -f "current.json" ] || touch current.json
   # check for jq
 if [[ `jq --version | echo $?` -eq 127 ]]; then
   [ "$OSTYPE" == "linux-gnu" ] && `apt-get install -y jq`
   [ "$OSTYPE" == "darwin"* ] && `brew install jq`
-  # Add `jq` to catalog
+  # Add `jq` to catalog ~ Maybe not?
 fi
+  # write to file
+i=0; while [ $i -lt $len ]; do
+  echo "Processing ${all[$i]}"
 
-# for soft in "${all[@]}"; do 
-#   echo "processing $soft" 
-# done
+((i++)); done
