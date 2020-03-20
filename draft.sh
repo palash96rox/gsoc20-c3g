4459fee4ab5c4b4ab26357d3641ee956f1a469dd
Palash A <pa334@cornell.edu>
2020-03-19 20:46:50 -0400

    soft obj

--- draft.sh
+++ draft.sh
@@ -2,7 +2,7 @@
 
 # start #
 [ $# -eq "1" ] && dir=$1 || echo "please supply parent directory"
-allsoft="$dir/software"
+allsoft="$dir/modulefiles/mugqic"
 
 # temp files and dirs #
 [ -d "json-temp" ] || mkdir json-temp
@@ -12,30 +12,36 @@ allsoft="$dir/software"
 declare -a all
 declare -a ver
 
-all=( $(ls $allsoft) ) && echo ${all[@]}
-for soft in ${all[@]}; do
-  ver=( $(ls -F "$allsoft/$soft" | grep /) )
-  curr=`echo TODO` # Check current version from modulefiles
-  # cat "$dir/modulefiles/mugqic/$soft/.version" \
-  #   | 
-done
-
-
-# create objects #
 declare -A obj
+
+all=( $(ls $allsoft) )
+for soft in ${all[@]}; do
+  ver=( $(ls -F "$allsoft/$soft") )
+  curr=$(cat "$allsoft/$soft/.version" | grep \" | awk '{print $NF}')
+  # .version format :: set ModulesVersion "<version-num>"
+  declare -A temp
+  temp[name]=$soft
+  temp[versions]=$ver
+  temp[current]=$curr
+  temp[notes]=""
+  obj+=([$soft]=$temp)
+  for i in ${temp[@]}; do
+    [ `declare -p ${1} 2> /dev/null | grep 'declare \-a'` ] \
+      && echo ${!i[@]} || echo $i
+  done
+  echo ""
+done
 
 
 # write to json #
 
+  # check for jq
 if [[ `jq --version | echo $?` -eq 127 ]]; then
   [ "$OSTYPE" == "linux-gnu" ] && `apt-get install -y jq`
   [ "$OSTYPE" == "darwin"* ] && `brew install jq`
   # Add `jq` to catalog
 fi
 
-for soft in "${all[@]}"; do 
-  echo "processing $soft" 
-done
-
-
-
+# for soft in "${all[@]}"; do 
+#   echo "processing $soft" 
+# done
